4f6e3d638693f9325b63e16e0e7a0357e0b4961f
mirko-verardo <mirko.verardo@gmail.com>
2026-02-02 00:36:39 +0100

    plots added for benchmarks

--- benchmark_group.py
+++ benchmark_group.py
@@ -1,8 +1,13 @@
-import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
 from pathlib import Path
+from matplotlib.patches import Patch
 
 
+
+def beautify_col(col: str) -> str:
+    return col.capitalize().replace("_", " ")
 
 if __name__ == "__main__":
     # read
@@ -46,50 +51,242 @@ if __name__ == "__main__":
     print(df)
 
     # general
+    barWidth = 0.3
+    br1 = np.arange(16) 
+    br2 = [i + barWidth for i in br1]
+    #br3 = [x + barWidth for x in br2]
+    #binsCyc = [1, 11, 21, 51, 171]
+    binsCyc = range(1, 172, 10)
+    binsCod = np.arange(0, 1.1, 0.1)
     cols = ["cyclomatic_complexity", "code_coverage"]
-    llms = ["anthropic", "google", "openai"]
-
-    # global plots
-    data = [ 
-        df["compilation_iteration"].dropna(), 
-        df["testing_iteration"].dropna() 
+    bins = [binsCyc, binsCod]
+    colors = ["orange", "blue"]
+    df_new = df.copy()
+    #df_new.loc[df_new["compilation_iteration"].isna(), "compilation_iteration"] = 16
+    df_new["compilation_iteration"] = df_new["compilation_iteration"].fillna(16)
+    df_new["testing_iteration"] = df_new["testing_iteration"].fillna(16)
+    df_new["cyclomatic_complexity"] = df_new["cyclomatic_complexity"].fillna(max(df_new["cyclomatic_complexity"]))
+    df_new["code_coverage"] = df_new["code_coverage"].fillna(0)
+    legend_elements = [
+        Patch(facecolor="tab:olive", label="Compilation"),
+        Patch(facecolor="tab:green", label="Testing"),
     ]
-    plt.boxplot(data, labels=["Compilation", "Testing"])
-    plt.ylabel("Iterations")
-    plt.title("Iterations by phase")
-    plt.show()
-    for col in cols:
-        plt.boxplot(df[col].dropna())
-        col_out = col.capitalize().replace("_", " ")
-        plt.ylabel(col_out)
-        plt.title(f"Distribution of {col_out}")
+    median_style = {"color": "tab:red"}
+
+    if False:
+        # Global plots
+        ## Boxplot
+        bp = plt.boxplot(
+            [df_new["compilation_iteration"].dropna(), df_new["testing_iteration"].dropna()], 
+            tick_labels=["Compilation", "Testing"], 
+            patch_artist=True,
+            medianprops=median_style
+        )
+        bp["boxes"][0].set_facecolor("tab:olive")
+        bp["boxes"][1].set_facecolor("tab:green")
+        plt.title("Compilation and Testing")
+        plt.ylabel("Iterations")
+        plt.legend(handles=legend_elements, loc="best")
+        plt.show()
+        ## Barplot
+        cmpl = [ df_new.loc[df_new["compilation_iteration"] == i + 1, "compilation_iteration"].count() for i in br1 ] 
+        test = [ df_new.loc[df_new["testing_iteration"] == i + 1, "testing_iteration"].count() for i in br1] 
+        plt.bar(br1, cmpl, color="tab:olive", edgecolor="grey", width=barWidth, label="Compilation") 
+        plt.bar(br2, test, color="tab:green", edgecolor="grey", width=barWidth, label="Testing") 
+        plt.title("Compilation and Testing")
+        plt.xlabel("Iterations")
+        plt.xticks([i + (barWidth/2) for i in br1], br1 + 1)
+        plt.legend(loc="best")
+        plt.show()
+        ## Boxplot
+        col = "cyclomatic_complexity"
+        bp = plt.boxplot(df_new[col].dropna(), tick_labels=[""], patch_artist=True, medianprops=median_style)
+        bp["boxes"][0].set_facecolor("tab:orange")
+        plt.title(beautify_col(col))
+        plt.show()
+        ## Histogram
+        plt.hist(df_new[col].dropna(), color="tab:orange", edgecolor="orange", bins=binsCyc)
+        plt.title(beautify_col(col))
+        plt.xticks(binsCyc) 
+        plt.show()
+        ## Boxplot
+        col = "code_coverage"
+        plt.boxplot(df_new[col].dropna(), tick_labels=[""], patch_artist=True, medianprops=median_style)
+        bp["boxes"][0].set_facecolor("tab:blue")
+        plt.title(beautify_col(col))
+        plt.show()
+        ## Histogram
+        plt.hist(df_new[col].dropna(), color="tab:blue", edgecolor="blue", bins=binsCod)
+        plt.title(beautify_col(col))
+        plt.xticks(binsCod)
         plt.show()
     
+    #raise SystemExit
+        
     # LLM plots
-    data = [ 
-        df.loc[df["llm"] == "anthropic", "compilation_iteration"].dropna(), 
-        df.loc[df["llm"] == "anthropic", "testing_iteration"].dropna(), 
-        df.loc[df["llm"] == "google", "compilation_iteration"].dropna(),
-        df.loc[df["llm"] == "google", "testing_iteration"].dropna(),
-        df.loc[df["llm"] == "openai", "compilation_iteration"].dropna(),
-        df.loc[df["llm"] == "openai", "testing_iteration"].dropna()
-    ]
-    plt.boxplot(data, 
-                labels=["Compilation anthropic", "Testing anthropic", "Compilation google", "Testing google", "Compilation openai", "Testing openai"], 
-                positions=[1, 2, 4, 5, 7, 8])
+    llms = ["Anthropic", "Google", "OpenAI"]
+    df_anth = df_new[df_new["llm"] == "anthropic"]
+    df_goog = df_new[df_new["llm"] == "google"]
+    df_open = df_new[df_new["llm"] == "openai"]
+    ## Boxplot
+    bp = plt.boxplot(
+        [ 
+            df_anth["compilation_iteration"].dropna(), 
+            df_anth["testing_iteration"].dropna(), 
+            df_goog["compilation_iteration"].dropna(),
+            df_goog["testing_iteration"].dropna(),
+            df_open["compilation_iteration"].dropna(),
+            df_open["testing_iteration"].dropna()
+        ], 
+        positions=[1, 2, 4, 5, 7, 8], 
+        patch_artist=True,
+        medianprops=median_style
+    )
+    # Set legend
+    for i in [0, 2, 4]:
+        bp["boxes"][i].set_facecolor("tab:olive")
+    for i in [1, 3, 5]:
+        bp["boxes"][i].set_facecolor("tab:green")
+    # Tick positions = centers of each pair
+    tick_positions = [1.5, 4.5, 7.5]
+    plt.xticks(tick_positions, llms)
+    plt.title("Compilation and Testing")
     plt.ylabel("Iterations")
-    plt.title("Iterations by LLM and phase")
+    plt.legend(handles=legend_elements, loc="best")
     plt.show()
-    
-    for col in cols:
-        data = [ 
-            df.loc[df["llm"] == "anthropic", col].dropna(), 
-            df.loc[df["llm"] == "google", col].dropna(),
-            df.loc[df["llm"] == "openai", col].dropna() 
+    ## Barplot
+    fig, axes = plt.subplots(3, 1, figsize=(12, 9))  # 3 rows, 1 column
+    data = [
+        df_anth,
+        df_goog,
+        df_open
+    ]
+    for i in range(len(data)):
+        df = data[i]
+        cmpl = [ df.loc[df["compilation_iteration"] == j + 1, "compilation_iteration"].count() for j in br1 ] 
+        test = [ df.loc[df["testing_iteration"] == j + 1, "testing_iteration"].count() for j in br1] 
+        axes[i].bar(br1, cmpl, color="tab:olive", edgecolor="grey", width=barWidth, label="Compilation") 
+        axes[i].bar(br2, test, color="tab:green", edgecolor="grey", width=barWidth, label="Testing") 
+        axes[i].set_title(llms[i])
+        #axes[i].set_xlabel("Iterations")
+        axes[i].set_xticks([j + (barWidth/2) for j in br1], br1 + 1)
+        axes[i].set_ylim(0, 150)
+        axes[i].legend(handles=legend_elements, loc="upper center")
+    axes[len(data) - 1].set_xlabel("Iterations")
+    plt.tight_layout()
+    plt.show()
+
+    ylims = [70, 140]
+    for i in range(len(cols)):
+        col = cols[i]
+        ticks = bins[i]
+        color = colors[i]
+        ## Boxplot
+        data = [
+            df_anth[col].dropna(), 
+            df_goog[col].dropna(),
+            df_open[col].dropna()
         ]
-        plt.boxplot(data, labels=llms)
-        col_out = col.capitalize().replace("_", " ")
-        plt.title(f"Distribution of {col_out}")
+        bp = plt.boxplot(
+            data,
+            tick_labels=llms,
+            patch_artist=True, 
+            medianprops=median_style
+        )
+        for j in range(len(data)):
+            bp["boxes"][j].set_facecolor(f"tab:{color}")
+        plt.title(beautify_col(col))
+        plt.show()
+        ## Histogram
+        fig, axes = plt.subplots(3, 1, figsize=(12, 9))  # 3 rows, 1 column
+        for j in range(len(data)):
+            axes[j].hist(data[j], color=f"tab:{color}", edgecolor=color, bins=ticks)
+            axes[j].set_title(llms[j])
+            axes[j].set_xticks(ticks)
+            axes[j].set_ylim(0, ylims[i])
+        axes[len(data) - 1].set_xlabel(beautify_col(col))
+        plt.tight_layout()
+        plt.show()
+
+    #raise SystemExit
+
+    # Architecture plots
+    archs = ["Single-agent", "Multi-agent"]
+    df_sa = df_new[df_new["type"] == "single_agent"]
+    df_ma = df_new[df_new["type"] == "multi_agent"]
+    data = [ 
+        df_sa["compilation_iteration"].dropna(), 
+        df_sa["testing_iteration"].dropna(), 
+        df_ma["compilation_iteration"].dropna(),
+        df_ma["testing_iteration"].dropna()
+    ]
+    ## Boxplot
+    bp = plt.boxplot(
+        data, 
+        positions=[1, 2, 4, 5], 
+        patch_artist=True,
+        medianprops=median_style
+    )
+    for i in [0, 2]:
+        bp["boxes"][i].set_facecolor("tab:olive")
+    for i in [1, 3]:
+        bp["boxes"][i].set_facecolor("tab:green")
+    tick_positions = [1.5, 4.5]
+    plt.xticks(tick_positions, archs)
+    plt.title("Compilation and Testing")
+    plt.ylabel("Iterations")
+    plt.legend(handles=legend_elements, loc="best")
+    plt.show()
+    ## Barplot
+    fig, axes = plt.subplots(2, 1, figsize=(12, 9))
+    data = [
+        df_sa,
+        df_ma
+    ]
+    for i in range(len(data)):
+        df = data[i]
+        cmpl = [ df.loc[df["compilation_iteration"] == j + 1, "compilation_iteration"].count() for j in br1 ] 
+        test = [ df.loc[df["testing_iteration"] == j + 1, "testing_iteration"].count() for j in br1] 
+        axes[i].bar(br1, cmpl, color="tab:olive", edgecolor="grey", width=barWidth, label="Compilation") 
+        axes[i].bar(br2, test, color="tab:green", edgecolor="grey", width=barWidth, label="Testing") 
+        axes[i].set_title(archs[i])
+        #axes[i].set_xlabel("Iterations")
+        axes[i].set_xticks([j + (barWidth/2) for j in br1], br1 + 1)
+        axes[i].set_ylim(0, 200)
+        axes[i].legend(handles=legend_elements, loc="upper center")
+    axes[len(data) - 1].set_xlabel("Iterations")
+    plt.tight_layout()
+    plt.show()
+
+    ylims = [120, 120]
+    for i in range(len(cols)):
+        col = cols[i]
+        ticks = bins[i]
+        color = colors[i]
+        ## Boxplot
+        data = [
+            df_sa[col].dropna(), 
+            df_ma[col].dropna()
+        ]
+        bp = plt.boxplot(
+            data,
+            tick_labels=archs,
+            patch_artist=True, 
+            medianprops=median_style
+        )
+        for j in range(len(data)):
+            bp["boxes"][j].set_facecolor(f"tab:{color}")
+        plt.title(beautify_col(col))
+        plt.show()
+        ## Histogram
+        fig, axes = plt.subplots(2, 1, figsize=(12, 9))
+        for j in range(len(data)):
+            axes[j].hist(data[j], color=f"tab:{color}", edgecolor=color, bins=ticks)
+            axes[j].set_title(archs[j])
+            axes[j].set_xticks(ticks)
+            axes[j].set_ylim(0, ylims[i])
+        axes[len(data) - 1].set_xlabel(beautify_col(col))
+        plt.tight_layout()
         plt.show()
 
     #df2 = df[(df["type"] == "single_agent") & (df["llm"] == "openai")]
@@ -105,7 +302,7 @@ if __name__ == "__main__":
     #for r in df2.itertuples():
     #    print(r.best_parser_folder)
 
-    #raise SystemExit
+    raise SystemExit
 
     groups = {
         "tfl": ["type", "file_format", "llm"],
