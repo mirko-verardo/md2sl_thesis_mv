56de808f36aeaf47d4a2e61a7f61365e736e71cf
mirko-verardo <mirko.verardo@gmail.com>
2026-02-07 13:31:00 +0100

    enhancing benchmark plots

--- benchmark_group.py
+++ benchmark_group.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import seaborn as sns
 import numpy as np
 import pandas as pd
 from pathlib import Path
@@ -7,7 +8,7 @@ from matplotlib.patches import Patch
 
 REPLACE_NA = False
 SHOW_PLOTS = True
-SHOW_ALL_BARPLOTS = False
+SHOW_BARPLOTS = False
 
 def beautify_col(col: str) -> str:
     return col.capitalize().replace("_", " ")
@@ -67,27 +68,27 @@ if __name__ == "__main__":
         br1 = np.arange(16) 
         br2 = [i + barWidth for i in br1]
         br3 = [x + barWidth for x in br2]
+        binsCyc = [1, 10, 20, 50]
         #binsCyc = [1, 11, 21, 51, 171]
-        binsCyc = range(1, 172, 10)
+        #binsCyc = range(1, 172, 10)
         binsCod = np.arange(0, 1.1, 0.1)
         bins = [binsCyc, binsCod]
-        colors = ["orange", "blue"]
         cols = ["cyclomatic_complexity", "code_coverage"]
         it_cols = ["compilation_iteration", "testing_iteration"]
-        legend_elements = [
-            Patch(facecolor="tab:olive", label="Compilation"),
-            Patch(facecolor="tab:green", label="Testing"),
+        legend_steps = [
+            Patch(facecolor="olive", label="Compilation", alpha=0.8),
+            Patch(facecolor="darkgreen", label="Testing", alpha=0.8),
         ]
         legend_llms = [
-            Patch(facecolor="tab:blue", label="Anthropic"),
-            Patch(facecolor="tab:red", label="Google"),
-            Patch(facecolor="tab:green", label="OpenAI"),
+            Patch(facecolor="blue", label="Anthropic", alpha=0.4),
+            Patch(facecolor="red", label="Google", alpha=0.4),
+            Patch(facecolor="green", label="OpenAI", alpha=0.4),
         ]
         legend_archs = [
-            Patch(facecolor="tab:red", label="Single-agent"),
-            Patch(facecolor="tab:blue", label="Multi-agent"),
+            Patch(facecolor="orange", label="Single-agent", alpha=0.6),
+            Patch(facecolor="purple", label="Multi-agent", alpha=0.6),
         ]
-        median_style = {"color": "tab:red"}
+        median_style = {"color": "tab:red", "linewidth": 1.5}
             
         # LLM plots
         llms = ["Anthropic", "Google", "OpenAI"]
@@ -95,74 +96,52 @@ if __name__ == "__main__":
         df_goog = df_new[df_new["llm"] == "google"]
         df_open = df_new[df_new["llm"] == "openai"]
         ## Boxplot
+        data = [ 
+            df_anth["compilation_iteration"].dropna(), 
+            df_anth["testing_iteration"].dropna(), 
+            df_goog["compilation_iteration"].dropna(),
+            df_goog["testing_iteration"].dropna(),
+            df_open["compilation_iteration"].dropna(),
+            df_open["testing_iteration"].dropna()
+        ]
         bp = plt.boxplot(
-            [ 
-                df_anth["compilation_iteration"].dropna(), 
-                df_anth["testing_iteration"].dropna(), 
-                df_goog["compilation_iteration"].dropna(),
-                df_goog["testing_iteration"].dropna(),
-                df_open["compilation_iteration"].dropna(),
-                df_open["testing_iteration"].dropna()
-            ], 
+            data, 
             positions=[1, 2, 4, 5, 7, 8], 
             patch_artist=True,
             medianprops=median_style
         )
         # Set legend
-        for i in [0, 2, 4]:
-            bp["boxes"][i].set_facecolor("tab:olive")
-        for i in [1, 3, 5]:
-            bp["boxes"][i].set_facecolor("tab:green")
+        for i in range(len(data)):
+            c = legend_steps[0 if (i % 2) == 0 else 1].get_facecolor()
+            bp["boxes"][i].set_facecolor(c)
         # Tick positions = centers of each pair
         tick_positions = [1.5, 4.5, 7.5]
         plt.xticks(tick_positions, llms)
         plt.title("Compilation and Testing")
         plt.ylabel("Iterations")
-        plt.legend(handles=legend_elements, loc="best")
+        plt.legend(handles=legend_steps, loc="best")
         plt.show()
-        if SHOW_ALL_BARPLOTS:
+        if SHOW_BARPLOTS:
             ## Barplot
-            fig, axes = plt.subplots(3, 1, figsize=(12, 9))  # 3 rows, 1 column
-            df_llms = [
-                df_anth,
-                df_goog,
-                df_open
-            ]
-            for i in range(len(df_llms)):
-                df_llm = df_llms[i]
-                cmpl = [ df_llm.loc[df_llm["compilation_iteration"] == j + 1, "compilation_iteration"].count() for j in br1 ] 
-                test = [ df_llm.loc[df_llm["testing_iteration"] == j + 1, "testing_iteration"].count() for j in br1] 
-                axes[i].bar(br1, cmpl, color="tab:olive", edgecolor="grey", width=barWidth, label="Compilation") 
-                axes[i].bar(br2, test, color="tab:green", edgecolor="grey", width=barWidth, label="Testing") 
-                axes[i].set_title(llms[i])
-                axes[i].set_xticks([j + (barWidth/2) for j in br1], br1 + 1)
+            fig, axes = plt.subplots(2, 1, figsize=(12, 9))  # 2 rows, 1 column
+            for i, it_col in enumerate(it_cols):
+                df_anth_col = [ df_anth.loc[df_anth[it_col] == j + 1, it_col].count() for j in br1 ]
+                df_goog_col = [ df_goog.loc[df_goog[it_col] == j + 1, it_col].count() for j in br1 ] 
+                df_open_col = [ df_open.loc[df_open[it_col] == j + 1, it_col].count() for j in br1 ] 
+                axes[i].bar(br1, df_anth_col, color=legend_llms[0].get_facecolor(), edgecolor="grey", width=barWidth, label="Anthropic") 
+                axes[i].bar(br2, df_goog_col, color=legend_llms[1].get_facecolor(), edgecolor="grey", width=barWidth, label="Google") 
+                axes[i].bar(br3, df_open_col, color=legend_llms[2].get_facecolor(), edgecolor="grey", width=barWidth, label="OpenAI") 
+                axes[i].set_title(beautify_col(it_col))
+                axes[i].set_xticks([j + barWidth for j in br1], br1 + 1)
                 axes[i].set_ylim(0, 200)
-                axes[i].legend(handles=legend_elements, loc="upper center")
+                axes[i].legend(handles=legend_llms, loc="upper center")
             axes[-1].set_xlabel("Iterations")
             plt.tight_layout()
             plt.show()
-        ## Barplot
-        fig, axes = plt.subplots(2, 1, figsize=(12, 9))  # 2 rows, 1 column
-        for i, it_col in enumerate(it_cols):
-            df_anth_col = [ df_anth.loc[df_anth[it_col] == j + 1, it_col].count() for j in br1 ]
-            df_goog_col = [ df_goog.loc[df_goog[it_col] == j + 1, it_col].count() for j in br1 ] 
-            df_open_col = [ df_open.loc[df_open[it_col] == j + 1, it_col].count() for j in br1 ] 
-            axes[i].bar(br1, df_anth_col, color="tab:blue", edgecolor="grey", width=barWidth, label="Anthropic") 
-            axes[i].bar(br2, df_goog_col, color="tab:red", edgecolor="grey", width=barWidth, label="Google") 
-            axes[i].bar(br3, df_open_col, color="tab:green", edgecolor="grey", width=barWidth, label="OpenAI") 
-            axes[i].set_title(beautify_col(it_col))
-            axes[i].set_xticks([j + barWidth for j in br1], br1 + 1)
-            axes[i].set_ylim(0, 200)
-            axes[i].legend(handles=legend_llms, loc="upper center")
-        axes[-1].set_xlabel("Iterations")
-        plt.tight_layout()
-        plt.show()
 
-        ylims = [100, 180]
         for i in range(len(cols)):
             col = cols[i]
             ticks = bins[i]
-            color = colors[i]
             ## Boxplot
             data = [
                 df_anth[col].dropna(), 
@@ -176,88 +155,66 @@ if __name__ == "__main__":
                 medianprops=median_style
             )
             for j in range(len(data)):
-                bp["boxes"][j].set_facecolor(f"tab:{color}")
+                c = legend_llms[j].get_facecolor()
+                bp["boxes"][j].set_facecolor(c)
             plt.title(beautify_col(col))
             plt.show()
             ## Histogram
-            fig, axes = plt.subplots(3, 1, figsize=(12, 9))  # 3 rows, 1 column
             for j in range(len(data)):
-                axes[j].hist(data[j], color=f"tab:{color}", edgecolor=color, bins=ticks)
-                axes[j].set_title(llms[j])
-                axes[j].set_xticks(ticks)
-                axes[j].set_ylim(0, ylims[i])
-            axes[-1].set_xlabel(beautify_col(col))
+                c = legend_llms[j].get_facecolor()
+                sns.histplot(data[j], color=c, edgecolor=c, label=llms[j], bins=ticks, kde=True)
+            plt.title(f"{beautify_col(col)} (KDE)")
+            plt.xlabel("")
+            plt.xticks(ticks)
+            plt.legend()
             plt.tight_layout()
             plt.show()
-
-        #raise SystemExit
 
         # Architecture plots
         archs = ["Single-agent", "Multi-agent"]
         df_sa = df_new[df_new["type"] == "single_agent"]
         df_ma = df_new[df_new["type"] == "multi_agent"]
         ## Boxplot
+        data = [
+            df_sa["compilation_iteration"].dropna(), 
+            df_sa["testing_iteration"].dropna(), 
+            df_ma["compilation_iteration"].dropna(),
+            df_ma["testing_iteration"].dropna()
+        ]
         bp = plt.boxplot(
-            [ 
-                df_sa["compilation_iteration"].dropna(), 
-                df_sa["testing_iteration"].dropna(), 
-                df_ma["compilation_iteration"].dropna(),
-                df_ma["testing_iteration"].dropna()
-            ], 
+            data, 
             positions=[1, 2, 4, 5], 
             patch_artist=True,
             medianprops=median_style
         )
-        for i in [0, 2]:
-            bp["boxes"][i].set_facecolor("tab:olive")
-        for i in [1, 3]:
-            bp["boxes"][i].set_facecolor("tab:green")
+        for i in range(len(data)):
+            c = legend_steps[0 if (i % 2) == 0 else 1].get_facecolor()
+            bp["boxes"][i].set_facecolor(c)
         tick_positions = [1.5, 4.5]
         plt.xticks(tick_positions, archs)
         plt.title("Compilation and Testing")
         plt.ylabel("Iterations")
-        plt.legend(handles=legend_elements, loc="best")
+        plt.legend(handles=legend_steps, loc="best")
         plt.show()
-        if SHOW_ALL_BARPLOTS:
+        if SHOW_BARPLOTS:
             ## Barplot
             fig, axes = plt.subplots(2, 1, figsize=(12, 9))
-            df_archs = [
-                df_sa,
-                df_ma
-            ]
-            for i in range(len(df_archs)):
-                df_arch = df_archs[i]
-                cmpl = [ df_arch.loc[df_arch["compilation_iteration"] == j + 1, "compilation_iteration"].count() for j in br1 ] 
-                test = [ df_arch.loc[df_arch["testing_iteration"] == j + 1, "testing_iteration"].count() for j in br1] 
-                axes[i].bar(br1, cmpl, color="tab:olive", edgecolor="grey", width=barWidth, label="Compilation") 
-                axes[i].bar(br2, test, color="tab:green", edgecolor="grey", width=barWidth, label="Testing") 
-                axes[i].set_title(archs[i])
+            for i, it_col in enumerate(it_cols):
+                df_sa_col = [ df_sa.loc[df_sa[it_col] == j + 1, it_col].count() for j in br1 ]
+                df_ma_col = [ df_ma.loc[df_ma[it_col] == j + 1, it_col].count() for j in br1 ] 
+                axes[i].bar(br1, df_sa_col, color=legend_archs[0].get_facecolor(), edgecolor="grey", width=barWidth, label="Single-agent") 
+                axes[i].bar(br2, df_ma_col, color=legend_archs[1].get_facecolor(), edgecolor="grey", width=barWidth, label="Multi-agent") 
+                axes[i].set_title(beautify_col(it_col))
                 axes[i].set_xticks([j + (barWidth/2) for j in br1], br1 + 1)
                 axes[i].set_ylim(0, 280)
-                axes[i].legend(handles=legend_elements, loc="upper center")
+                axes[i].legend(handles=legend_archs, loc="upper center")
             axes[-1].set_xlabel("Iterations")
             plt.tight_layout()
             plt.show()
-        ## Barplot
-        fig, axes = plt.subplots(2, 1, figsize=(12, 9))
-        for i, it_col in enumerate(it_cols):
-            df_sa_col = [ df_sa.loc[df_sa[it_col] == j + 1, it_col].count() for j in br1 ]
-            df_ma_col = [ df_ma.loc[df_ma[it_col] == j + 1, it_col].count() for j in br1 ] 
-            axes[i].bar(br1, df_sa_col, color="tab:red", edgecolor="grey", width=barWidth, label="Single-agent") 
-            axes[i].bar(br2, df_ma_col, color="tab:blue", edgecolor="grey", width=barWidth, label="Multi-agent") 
-            axes[i].set_title(beautify_col(it_col))
-            axes[i].set_xticks([j + (barWidth/2) for j in br1], br1 + 1)
-            axes[i].set_ylim(0, 280)
-            axes[i].legend(handles=legend_archs, loc="upper center")
-        axes[-1].set_xlabel("Iterations")
-        plt.tight_layout()
-        plt.show()
 
-        ylims = [160, 140]
         for i in range(len(cols)):
             col = cols[i]
             ticks = bins[i]
-            color = colors[i]
             ## Boxplot
             data = [
                 df_sa[col].dropna(), 
@@ -270,17 +227,18 @@ if __name__ == "__main__":
                 medianprops=median_style
             )
             for j in range(len(data)):
-                bp["boxes"][j].set_facecolor(f"tab:{color}")
+                c = legend_archs[j].get_facecolor()
+                bp["boxes"][j].set_facecolor(c)
             plt.title(beautify_col(col))
             plt.show()
             ## Histogram
-            fig, axes = plt.subplots(2, 1, figsize=(12, 9))
             for j in range(len(data)):
-                axes[j].hist(data[j], color=f"tab:{color}", edgecolor=color, bins=ticks)
-                axes[j].set_title(archs[j])
-                axes[j].set_xticks(ticks)
-                axes[j].set_ylim(0, ylims[i])
-            axes[-1].set_xlabel(beautify_col(col))
+                c = legend_archs[j].get_facecolor()
+                sns.histplot(data[j], color=c, edgecolor=c, label=archs[j], bins=ticks, kde=True)
+            plt.title(f"{beautify_col(col)} (KDE)")
+            plt.xlabel("")
+            plt.xticks(ticks)
+            plt.legend()
             plt.tight_layout()
             plt.show()
 
